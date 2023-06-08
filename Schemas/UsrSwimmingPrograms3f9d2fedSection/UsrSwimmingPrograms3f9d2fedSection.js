efdd54b80bc6a3ed95c354e618c7a98023e03cc5
maxim <maxim03nock@mail.ru>
2023-06-08 21:55:54 +0300

    Добавлен новый раздел, привязаны раздел,рабочее место, и группы пользователей

---
+++ Schemas/UsrSwimmingPrograms3f9d2fedSection/UsrSwimmingPrograms3f9d2fedSection.js
@@ -0,0 +1,8 @@
+define("UsrSwimmingPrograms3f9d2fedSection", [], function() {
+	return {
+		entitySchemaName: "UsrSwimmingPrograms",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
